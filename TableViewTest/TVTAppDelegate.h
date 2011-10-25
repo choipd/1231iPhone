936f0b1b76698f392085c05cf914df8c6848522a
Myungjin Choi <choipd@choipd.net>
2011-10-26 00:51:09 +0900

    Initial Commit

---
+++ TableViewTest/TVTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TVTAppDelegate.h
+//  TableViewTest
+//
+//  Created by Myungjin Choi on 11. 10. 26..
+//  Copyright (c) 2011년 Insol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TVTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TableViewTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TableViewTest
+//
+//  Created by Myungjin Choi on 11. 10. 26..
+//  Copyright (c) 2011년 Insol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TVTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TVTAppDelegate class]));
+    }
+}
